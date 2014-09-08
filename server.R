966c81debf5973eaab1b0beca92b2a1029e58b84
jaysha101 <jaysha101@yahoo.com>
2014-09-07 19:44:49 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,25 +1,36 @@
-smpl<-read.csv("C:/App-Titanic/data/smpl.csv")
+smpl<-read.csv("data/smpl - Copy.csv")
 
 library(shiny)
+library(ggplot2)
 
 Survival <- function(Gender, Sub, Age, Sib, Class)
+  
+#Weights for the variables from Logistic Regression
+  {Terms <- 16.595963 + Gender + Sub + (-0.039330*Age) + (-0.327496*Sib) + Class
+  100.00*(exp(Terms) / (1 + exp(Terms)))
+  }
+
+#Space to plot the charts
+shinyServer(function(input, output, session) 
   {
-  Terms <- 3.57698 + Gender + Sub + (-0.03184*Age) + (-0.29287*Sib) + Class
-  exp(Terms) / (1 + exp(Terms))
-}
-shinyServer(
-  function(input, output, session) {
+  
+  output$myplot<-renderPlot({ 
     
-    
-    output$myplot<-renderPlot({        
-      plot(smpl[c(input$cname1)])
-      #, input$cname2)])
-    })
-    
+    c <- ggplot(smpl, aes_string(x=input$cname1)) + geom_bar(fill="grey50",width=.8) 
+    print(c)
+  })
    
-    output$Sib <- renderPrint({Height(input$Sib)})
-    output$Age <- renderPrint({input$Age})
-    output$SurvivalRisk <- renderPrint({round(Survival(Gender=as.numeric(input$Gender), 
+   
+#ggplot for the percent of total
+  output$myggplot<-renderPlot({ 
+      p <- ggplot(smpl,aes_string(x=input$cname1,fill=input$cname2)) + geom_bar(colour="grey50",position="fill",width=.8)+xlab(input$cname1)+ylab(input$cname2)
+      print(p)
+      })
+    
+#Survival prediction for the calculator
+output$Sib <- renderPrint({Height(input$Sib)})
+output$Age <- renderPrint({input$Age})
+output$SurvivalRisk <- renderPrint({round(Survival(Gender=as.numeric(input$Gender), 
                                                        Sub=as.numeric(input$Sub),
                                                        Class=as.numeric(input$Class),
                                                        Age=input$Age, 
@@ -27,5 +38,4 @@ shinyServer(
                                                        Sib=input$Sib
                                                        
                                                        ),2)})
-  }
-)
+})
